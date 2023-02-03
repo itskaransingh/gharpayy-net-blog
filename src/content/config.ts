5800e07bdede95e77ebdfa99b25899356d80c5b6
Karan Singh <karanrsingh2662006@gmail.com>
2023-02-03 17:40:46 +0530

    solved some issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,8 +4,8 @@ const BlogPosts = defineCollection({
   schema: {
     title: z.string(),
     excerpt: z.string(),
-    category: z.string().trim(),
-    author: z.string().trim(),
+    // category: z.string().trim(),
+    // author: z.string().trim(),
     draft: z.boolean().optional(),
     tags: z.array(z.string()),
     image: z.string().optional(),
