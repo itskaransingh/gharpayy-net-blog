6a78d97798e54265e9c70ec383dfbe340a8e3aa2
Karan Singh <karanrsingh2662006@gmail.com>
2023-02-04 21:00:09 +0530

    improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,20 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import { remarkReadingTime } from "./src/utils/all";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://gharpayy.com",
+  site: "https://gharpayyblog.netlify.app",
   experimental: {
-    contentCollections: true,
+    contentCollections: true
   },
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap()]
 });
